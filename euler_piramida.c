110b900e4396fa4912af95443eb8d31911b23556
BIGbadEL <greg090912@gmail.com>
2018-04-21 20:43:49 +0200

    using dynamic prigraming

--- euler_piramida.c
+++ euler_piramida.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 100
 
-int max_suma(int tab[15][15], int inx, int b){
+int max_suma(int tab[N][N], int inx, int b){
+    static int wynik[N][N] = {0};
+
+
+
+    wynik[0][0] = tab[0][0];
     int max = 0, max2 = 0;
-    if(inx <= 0) return max=tab[0][0];
+    if(inx < 0) return 0;
+    if(wynik[inx][b] != 0) return wynik[inx][b];
     if(tab[inx][b] == 0) return 0;
 
 
@@ -11,29 +18,36 @@ int max_suma(int tab[15][15], int inx, int b){
     max2 = max_suma(tab, inx-1,b)+tab[inx][b];
 
     if(max>max2){
-        return max;
+        wynik[inx][b] = max;
     }else {
-        return max2;
+        wynik[inx][b] = max2;
     }
+    return wynik[inx][b];
 }
 
 int main(){
-    int tab[15][15];
-    for(int i = 0; i<15; i++){
-        for(int j = 0; j<15; j++){
+    int max = 0;
+    int tab[N][N] = {0};
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<=i; j++){
             scanf("%d", *(tab+i)+j);
         }
     }
 
-    for(int i = 0; i<15; i++){
-        for(int j = 0; j<15; j++){
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<N; j++){
             printf("%4d ", *(*(tab+i)+j));
         }
         printf("\n");
     }
-    for(int i = 0; i<15; i++){
-        printf("max: %d\n", max_suma(tab,14,i));
+    int f;
+    for(int i = 0; i<N; i++){
+        f = max_suma(tab,N-1,i);
+        if(f>max){
+            max = f;
+        }
     }
+    printf("%d\n", max);
 
 
     printf("\n");
